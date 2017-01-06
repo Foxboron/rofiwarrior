1ec786684c65b2e08984fab3ce44ee09cd19d32a
Morten Linderud <morten@linderud.pw>
2017-01-06 01:49:33 +0100

    More features
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- rofiwarrior.py
+++ rofiwarrior.py
@@ -2,6 +2,8 @@
 
 import sys
 
+import dateutil.parser
+
 from rofi import Rofi
 from taskw import TaskWarrior
 
@@ -12,30 +14,53 @@ class RofiWarrior:
         self.taskw = TaskWarrior()
 
         self.keybinds = {
-                'exit_hotkeys': ('Alt+F4','Control+q'),
                 "key1": ('Alt+t','list todo'),
                 "key2": ('Alt+a','add todo'),
+                "key3": ('Alt+d','due dates'),
                 }
 
         self.actions = [
                 None, # Padding
                 self.list_todo,
                 self.add_todo,
+                self.list_tasks_due,
                 sys.exit
                 ]
 
 
-    def list_todo(self):
-        tasks = self.taskw.load_tasks()
-        ret_tasks = []
-        for i in tasks["pending"]:
-            ret_tasks.append(i["description"])
-
-        self._callback(items=ret_tasks)
+    def list_todo(self, tasks=None):
+        if not tasks:
+            tasks = self.taskw.load_tasks()["pending"]
+        items = []
+        for i in tasks:
+            items.append(i["description"])
+        l = self._callback(items)
 
     def add_todo(self):
         task = self.rofi.text_entry("Add task: ")
+        if not task:
+            self.list_todo()
         self.taskw._execute("add", *task.split())
+
+    def list_tasks_due(self):
+        tasks = self.taskw.load_tasks()
+        date_items = {}
+        items = []
+        
+        # Wow, such horrible
+        for i in tasks["pending"]:
+            if "due" in i.keys():
+                date = dateutil.parser.parse(i["due"])
+                date = str(date).split("+")[0]
+                if date_items.get(date):
+                    date_items[date].append(i)
+                else:
+                    date_items[date] = [i]
+
+        items = sorted(date_items.keys())
+        ret = self._callback(items)
+        tasks = date_items[items[ret]]
+        self.list_todo(tasks)
 
     def _callback(self, items=[], prompt=">> "):
         ret = self.rofi.select(prompt, items, **self.keybinds)
